4fdbb4266a47522f27bd464c521a5601fcf60cf5
mauegraus <i.klatt@mailbox.org>
2020-05-21 15:23:58 +0200

    read in JSON process mappings using HttpClient module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  processMappings: any = [];
+// define a public property for the process mapping
+  /* public processMapping: {
+    processId: string,
+    name: string,
+    productCollection: [
+      string
+    ],
+    inputs: [
+      {
+        inputType: string,
+        processInputId: string
+      },
+    ]
+  }[] = processMappings;
+ */
+
+
   loadedFeature = 'job-overview';
 
   onNavigate(selectedFeature: string) {
     this.loadedFeature = selectedFeature;
   }
+
+  constructor(private httpClient: HttpClient){}
+
+   ngOnInit(){
+    this.httpClient.get('assets/processMappings.json').subscribe(data => {
+      console.log(data);
+      this.processMappings = data;
+    });
+   }
 }
