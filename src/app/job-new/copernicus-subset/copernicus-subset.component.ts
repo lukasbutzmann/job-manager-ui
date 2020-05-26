14534c9449eb40926b795b6dfe6edfdc9bf2d947
mauegraus <i.klatt@mailbox.org>
2020-05-26 14:15:56 +0200

    enable registering more than one of the same component in the new-job form object by using input directive

--- src/app/job-new/copernicus-subset/copernicus-subset.component.ts
+++ src/app/job-new/copernicus-subset/copernicus-subset.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-copernicus-subset',
   templateUrl: './copernicus-subset.component.html',
-  styleUrls: ['./copernicus-subset.component.css']
+  styleUrls: ['./copernicus-subset.component.css'],
+  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
 })
 export class CopernicusSubsetComponent implements OnInit {
+@Input() index: number;
 
   constructor() { }
 
--- src/app/job-new/static-subset/static-subset.component.ts
+++ src/app/job-new/static-subset/static-subset.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-static-subset',
   templateUrl: './static-subset.component.html',
-  styleUrls: ['./static-subset.component.css']
+  styleUrls: ['./static-subset.component.css'],
+  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
 })
 export class StaticSubsetComponent implements OnInit {
+@Input() index: number;
 
   constructor() { }
 
