b0cd70a1c3bf9ad55b5e154eb00e4aa5766a9f84
mauegraus <i.klatt@mailbox.org>
2020-05-29 17:03:50 +0200

    add dependencies between input fields, so that a choice in on input field is reflected in other input field options

--- src/app/job-new/copernicus-subset/copernicus-subset.component.ts
+++ src/app/job-new/copernicus-subset/copernicus-subset.component.ts
@@ -11,6 +11,8 @@ export class CopernicusSubsetComponent implements OnInit {
   @Input() index: number;
   sourceType = 'CopernicusSubsetDefinition';
   @Input() defaultProcessID: string;
+  satellite = '';
+  productType: string;
 
   constructor() { }
 
